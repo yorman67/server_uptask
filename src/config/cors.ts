bbd6e85207d1ad27ea9454cd2b4996c7642630cd
Yorman <yorman.p.martinez@gmail.com>
2024-06-28 20:49:13 -0500

    cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -3,9 +3,12 @@ import { CorsOptions } from "cors";
 export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
         const whiteList = [process.env.FRONTEND_URL]
+        console.log(whiteList)
         if(whiteList.includes(origin)){
             callback(null, true);
         } else {
+            console.log("daskjkldasjdklasjdaklñsdjñjdñk------------------------------------------")
+            console.log(whiteList)
             callback(new Error("Not allowed by CORS"));
         }
     }
