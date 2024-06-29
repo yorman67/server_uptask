d57b2c49640d73597ab34e8fb51cbb952aa1d12f
Yorman <yorman.p.martinez@gmail.com>
2024-06-28 21:23:48 -0500

    cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,13 +1,14 @@
 import { CorsOptions } from "cors";
+import { config } from "dotenv";
+config()
+
 
 export const corsConfig: CorsOptions = {
     origin: function (origin, callback) {
         const whiteList = [process.env.FRONTEND_URL]
-        console.log(whiteList)
         if(whiteList.includes(origin)){
             callback(null, true);
         } else {
-            console.log("daskjkldasjdklasjdaklñsdjñjdñk------------------------------------------")
             console.log(whiteList)
             callback(new Error("Not allowed by CORS"));
         }
